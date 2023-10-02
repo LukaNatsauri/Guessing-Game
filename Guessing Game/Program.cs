d37bfc477bedb64869431c777dd9053579a14952
Pc World <Pc World@DESKTOP-S1ILAGK>
2023-10-02 16:40:33 +0400

    Add project files.

---
+++ Guessing Game/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace GuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while(guess != secretWord && !outOfGuesses) 
+            {
+                if (guessCount < guessLimit) 
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                } else
+                {
+                    outOfGuesses = true;
+                }
+                
+            }
+            if (outOfGuesses)
+            {
+                Console.Write("You Lose!");
+            }
+            else {
+                Console.Write("You Win!");
+            }
+            
+
+            Console.ReadLine();
+        }
+
+        
+
+    }
+}
